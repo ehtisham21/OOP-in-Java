98430bbb815b4aa05743b2e121214b0b8ccb831e
Ehtisham Butt <buttehtisham123@gmail.com>
2022-08-24 13:40:30 +0500

    Single Inheritance with Constructor

--- src/main/java/Inheritance.java
+++ src/main/java/Inheritance.java
@@ -1,10 +1,19 @@
+//This Example is for Single Inheritance
 class SimpleInheritance {
+    //Constructor
+    SimpleInheritance() {
+         System.out.println("\nI am Parent Class Constrcutor");
+    }
     void parent(){
         System.out.println("I am the Parent Class");
     }
 }
 
 class student extends SimpleInheritance{
+     //Constructor
+    student() {
+         System.out.println("\nI am Child Class Constrcutor");
+    }
     void child(){
         System.out.println("I am the Child Class");
     }
